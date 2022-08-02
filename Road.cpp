986aa9fc27027d8600cd6aed185e3b259397863c
Taiaba Rahat Ritu <86781069+Taiaba-Rahat-Ritu@users.noreply.github.com>
2022-08-02 08:51:14 +0600

    Upload code (Road)

---
+++ Road.cpp
@@ -0,0 +1,221 @@
+///Draw_Road///
+//day
+void drawroad()
+{
+     ///road end1
+    Quad(-1.0f,0.0f,-0.75f,0.0f,-0.75f,-0.03f,-1.0f,-0.03f,1.0f,1.0f,0.0f);
+        glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-1.0f,0.0f,-0.75f,0.0f,-0.75f,-0.03f,-1.0f,-0.03f,1.0f,1.0f,0.0f);
+        glLoadIdentity();
+        glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-1.0f,0.0f,-0.75f,0.0f,-0.75f,-0.03f,-1.0f,-0.03f,1.0f,1.0f,0.0f);
+        glLoadIdentity();
+        glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-1.0f,0.0f,-0.75f,0.0f,-0.75f,-0.03f,-1.0f,-0.03f,1.0f,1.0f,0.0f);
+        glLoadIdentity();
+
+    Quad(-0.75f,0.0f,-0.5f,0.0f,-0.5f,-0.03f,-0.75f,-0.03f,1.0f,1.0f,1.0f);
+        glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-0.75f,0.0f,-0.5f,0.0f,-0.5f,-0.03f,-0.75f,-0.03f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-0.75f,0.0f,-0.5f,0.0f,-0.5f,-0.03f,-0.75f,-0.03f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-0.75f,0.0f,-0.5f,0.0f,-0.5f,-0.03f,-0.75f,-0.03f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+
+    ///road1
+    Quad(-1.0f,-0.03f,1.0f,-0.03f,1.0f,-0.375f,-1.0f,-0.375,0.169f,0.169f,0.169f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,1.0f,1.0f,1.0f);
+
+        glTranslatef(-0.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(0.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(0.75f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.75f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+
+
+
+    ///road2
+    Quad(-1.0f,-0.625f,1.0f,-0.625f,1.0f,-1.0f,-1.0f,-1.0,0.169f,0.169f,0.169f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,1.0f,1.0f,1.0f);
+
+        glTranslatef(-0.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,1.0f,1.0f,1.0f);
+    glLoadIdentity();
+        glTranslatef(0.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(0.75f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.75f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+
+    ///road end2
+    Quad(-1.0f,-0.97f,-0.75f,-0.97f,-0.75f,-1.0f,-1.0f,-1.0,1.0f,1.0f,0.0f);
+        glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-1.0f,-0.97f,-0.75f,-0.97f,-0.75f,-1.0f,-1.0f,-1.0,1.0f,1.0f,0.0f);
+        glLoadIdentity();
+        glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-1.0f,-0.97f,-0.75f,-0.97f,-0.75f,-1.0f,-1.0f,-1.0,1.0f,1.0f,0.0f);
+        glLoadIdentity();
+        glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-1.0f,-0.97f,-0.75f,-0.97f,-0.75f,-1.0f,-1.0f,-1.0,1.0f,1.0f,0.0f);
+        glLoadIdentity();
+    Quad(-0.75f,-0.97f,-0.5f,-0.97f,-0.5f,-1.0f,-0.75f,-1.0f,1.0f,1.0f,1.0f);
+        glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-0.75f,-0.97f,-0.5f,-0.97f,-0.5f,-1.0f,-0.75f,-1.0f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-0.75f,-0.97f,-0.5f,-0.97f,-0.5f,-1.0f,-0.75f,-1.0f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+        glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-0.75f,-0.97f,-0.5f,-0.97f,-0.5f,-1.0f,-0.75f,-1.0f,1.0f,1.0f,1.0f);
+        glLoadIdentity();
+
+}
+
+
+//night
+void drawroadnight()
+{
+     ///road end1
+    Quad(-1.0f,0.0f,-0.75f,0.0f,-0.75f,-0.03f,-1.0f,-0.03,0.8f,0.8f,0.0f);
+    glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-1.0f,0.0f,-0.75f,0.0f,-0.75f,-0.03f,-1.0f,-0.03,0.8f,0.8f,0.0f);
+    glLoadIdentity();
+    glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-1.0f,0.0f,-0.75f,0.0f,-0.75f,-0.03f,-1.0f,-0.03,0.8f,0.8f,0.0f);
+    glLoadIdentity();
+    glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-1.0f,0.0f,-0.75f,0.0f,-0.75f,-0.03f,-1.0f,-0.03,0.8f,0.8f,0.0f);
+    glLoadIdentity();
+
+    Quad(-0.75f,0.0f,-0.5f,0.0f,-0.5f,-0.03f,-0.75f,-0.03,0.8f,0.8f,0.8f);
+    glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-0.75f,0.0f,-0.5f,0.0f,-0.5f,-0.03f,-0.75f,-0.03,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-0.75f,0.0f,-0.5f,0.0f,-0.5f,-0.03f,-0.75f,-0.03,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-0.75f,0.0f,-0.5f,0.0f,-0.5f,-0.03f,-0.75f,-0.03,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+
+
+    ///road1
+    Quad(-1.0f,-0.03f,1.0f,-0.03f,1.0f,-0.375f,-1.0f,-0.375,0.1f,0.1f,0.1f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,0.8f,0.8f,0.8f);
+
+    glTranslatef(-0.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(0.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(0.75f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.75f,0.0f, 0.0f);
+    Quad(-0.825f,-0.175f,-0.675f,-0.175f,-0.675f,-0.2f,-0.825f,-0.2,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+
+     ///road2
+    Quad(-1.0f,-0.625f,1.0f,-0.625f,1.0f,-1.0f,-1.0f,-1.0,0.1f,0.1f,0.1f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,0.8f,0.8f,0.8f);
+
+    glTranslatef(-0.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(0.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(0.75f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.25f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.75f,0.0f, 0.0f);
+    Quad(-0.825f,-0.8f,-0.675f,-0.8f,-0.675f,-0.825f,-0.825f,-0.825f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+
+    ///road end2
+    Quad(-1.0f,-0.97f,-0.75f,-0.97f,-0.75f,-1.0f,-1.0f,-1.0,0.8f,0.8f,0.0f);
+    glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-1.0f,-0.97f,-0.75f,-0.97f,-0.75f,-1.0f,-1.0f,-1.0,0.8f,0.8f,0.0f);
+    glLoadIdentity();
+    glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-1.0f,-0.97f,-0.75f,-0.97f,-0.75f,-1.0f,-1.0f,-1.0,0.8f,0.8f,0.0f);
+    glLoadIdentity();
+    glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-1.0f,-0.97f,-0.75f,-0.97f,-0.75f,-1.0f,-1.0f,-1.0,0.8f,0.8f,0.0f);
+    glLoadIdentity();
+
+    Quad(-0.75f,-0.97f,-0.5f,-0.97f,-0.5f,-1.0f,-0.75f,-1.0f,0.8f,0.8f,0.8f);
+    glTranslatef(0.5f,0.0f, 0.0f);
+    Quad(-0.75f,-0.97f,-0.5f,-0.97f,-0.5f,-1.0f,-0.75f,-1.0f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.0f,0.0f, 0.0f);
+    Quad(-0.75f,-0.97f,-0.5f,-0.97f,-0.5f,-1.0f,-0.75f,-1.0f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+    glTranslatef(1.5f,0.0f, 0.0f);
+    Quad(-0.75f,-0.97f,-0.5f,-0.97f,-0.5f,-1.0f,-0.75f,-1.0f,0.8f,0.8f,0.8f);
+    glLoadIdentity();
+
+}
